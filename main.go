afaeb98444c11f96e1971aa22f517a2f6459224b
Anandharsh275 <anandharsh275@gmail.com>
2024-05-29 23:30:17 +0530

    committing first code simple server

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+	"runtime"
+	"time"
+)
+
+func main() {
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("Hello World")
+		time.Sleep(5 * time.Second)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
+	fmt.Println("Starting server on :8080")
+	go openBrowser("http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+
+}
+func openBrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+
+	if err != nil {
+		panic(err)
+	}
+}
