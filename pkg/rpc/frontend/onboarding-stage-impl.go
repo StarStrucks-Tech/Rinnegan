6216e5437d2b786546723db932a0308312bab897
Shubham Tiwari <125534462+ShubhamTiwari55@users.noreply.github.com>
2024-07-11 01:02:00 +0530

    Update onboarding-stage-impl.go

--- pkg/rpc/frontend/onboarding-stage-impl.go
+++ pkg/rpc/frontend/onboarding-stage-impl.go
@@ -8,13 +8,17 @@ import (
 	"context"
 )
 
+// Server is the struct that implements the FrontendServiceServer interface.
 type Server struct {
 	pb.UnimplementedFrontendServiceServer
 }
 
+// GetCurrentOnboardingStage handles the request to get the current onboarding stage.
+// It takes a context and an empty request as parameters, and returns a response containing the current onboarding stage.
 func (s *Server) GetCurrentOnboardingStage(
 	ctx context.Context,
 	_ *generic.EmptyRequest) (*onboarding.GetCurrentOnboardingStageResponse, error) {
+	// Return a response with the current onboarding stage set to EMAIL_VERIFICATION.
 	return &onboarding.GetCurrentOnboardingStageResponse{
 		CurrentStage: enums.OnboardingStage_ONBOARDING_STAGE_EMAIL_VERIFICATION,
 	}, nil
